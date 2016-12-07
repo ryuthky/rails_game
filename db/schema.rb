fa01a617a7c0a0bf7554b0faa20dc0115cc1a976
ryuthky <ryu.ayurose@gmail.com>
2016-12-07 08:23:58 +0000

    change db postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161207060750) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.datetime "pots_date"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20161207060750) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "articles", ["user_id"], name: "index_articles_on_user_id"
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -32,4 +35,5 @@ ActiveRecord::Schema.define(version: 20161207060750) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
